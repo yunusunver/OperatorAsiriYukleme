0aafe6ba0bd181530ffbbe72c6c7b4c7325fe9cf
yunusunver <yunus.unver1995@gmail.com>
2017-04-06 12:20:56 +0300

    Yunus add

--- OperatorAsiriYukleme/Program.cs
+++ OperatorAsiriYukleme/Program.cs
@@ -218,7 +218,7 @@ namespace OperatorAsiriYukleme
             DiziCarp(boyut,carpma);
             DiziEsitmi(dizi1,dizi2);
 
-
+            Console.WriteLine("Yunus");
 
             Console.ReadKey();
 
